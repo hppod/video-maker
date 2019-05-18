21ecdbb5191bbd24c4ebcc92f9c25bb2d6021d3e
hppod <hppodgornik@gmail.com>
2019-05-18 17:46:45 -0300

    Implement funciton download all images

--- robots/image.js
+++ robots/image.js
@@ -6,9 +6,10 @@ const googleSearchCredentials = require('./../credentials/google-search.json')
 async function robot() {
     const content = state.load()
 
-    await fetchImagesOfAllSentences(content)
+    // await fetchImagesOfAllSentences(content)
+    await downloadAllImages(content)
 
-    state.save(content)
+    // state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
@@ -33,6 +34,30 @@ async function robot() {
         })
 
         return imagesUrl
+    }
+
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex]
+
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
+                        throw new Error('Imagem já foi baixada. Tentando baixar outra imagem....')
+                    }
+
+                    //await downloadImage()
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso ${imageUrl}`)
+                    break
+                } catch (error) {
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar a imagem (${imageUrl}): ${error}`)
+                }
+            }
+        }
     }
 }
 
