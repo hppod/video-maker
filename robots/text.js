ee0d2b5e25ed614731b850f9b0422e65bc7a493c
hppod <hppodgornik@gmail.com>
2019-05-18 16:24:42 -0300

    Add input robot and state robot

--- robots/text.js
+++ robots/text.js
@@ -4,18 +4,23 @@ const sentenceBoundaryDetection = require('sbd')
 const watsonApiKey = require('./../credentials/watson-nlu.json')
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js')
 
-var nlu = new NaturalLanguageUnderstandingV1({
+const nlu = new NaturalLanguageUnderstandingV1({
     iam_apikey: watsonApiKey.apikey,
     version: '2018-04-05',
     url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
 });
 
-async function robot(content) {
+const state = require('./state.js')
+
+async function robot() {
+    const content = state.load()
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
     limitMaximumSentences(content)
     await fetchKeywordsOfAllSentences(content)
+
+    state.save(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaKey)
