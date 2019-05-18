272043680a4139fed55b9e19a6a853ac9839f859
hppod <hppodgornik@gmail.com>
2019-05-18 14:30:29 -0300

    Remove date in parentheses

--- robots/text.js
+++ robots/text.js
@@ -16,35 +16,27 @@ async function robot(content) {
     }
 
     function sanitizeContent(content) {
-        const withoutBlankLines = removeBlankLines(content.sourceContentOriginal)
-        const withoutMarkdown = removeMarkdown(withoutBlankLines)
-        console.log(withoutMarkdown)
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDateInParentheses(withoutBlankLinesAndMarkdown)
+        console.log(withoutDatesInParentheses)
 
-        function removeBlankLines(text) {
+        function removeBlankLinesAndMarkdown(text) {
             const allLines = text.split('\n')
 
-            const withoutBlankLines = allLines.filter((line) => {
-                if (line.trim().length === 0) {
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
                     return false
                 }
 
                 return true
             })
 
-            return withoutBlankLines
+            return withoutBlankLinesAndMarkdown.join(' ')
         }
     }
 
-    function removeMarkdown(lines) {
-        const withoutMarkdown = lines.filter((line) => {
-            if (line.trim().startsWith('=')) {
-                return false
-            }
-
-            return true
-        })
-
-        return withoutMarkdown
+    function removeDateInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
     }
 }
 
