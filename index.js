4353c2f26e9da05e83e6de6194654b2d7a983f40
hppod <hppodgornik@gmail.com>
2019-05-18 13:24:51 -0300

    Return prefix text

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function start() {
     function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
 
-        console.log(selectedPrefixIndex)
+        return selectedPrefixText
     }
 
     console.log(content)
